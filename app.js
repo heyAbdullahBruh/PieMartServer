0389f060649219cd931ff59f29ceb8771633de87
Abu Sayed <abus04651@gmail.com>
2024-11-04 20:30:59 +0600

    Project initialize

---
+++ app.js
@@ -0,0 +1,48 @@
+const express= require('express');
+const cors= require('cors');
+
+const app =express();
+
+// middleware duction calling---->
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+
+
+// home route
+app.get('/',(req,res)=>{
+    try {
+        return res.status(200).sendFile(__dirname+'/view/index.html');
+    } catch (error) {
+        return res.status(404).json({status:false,message:`Something went worng : ${error.message}`}); 
+    }
+});
+
+
+
+
+// false route's error definder
+app.use((req,res,next)=>{
+    return res.status(404).json({status:false,message:'the route is not found'});
+});
+
+
+// server error definder
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500;
+    return res.status(statusCode).json({
+      success:false,
+      message: error.message || 'Internal Server Error'
+    });
+});
+
+
+
+
+
+
+
+
+module.exports =app;
